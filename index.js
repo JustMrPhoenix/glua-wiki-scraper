f11a960997d731917d25c7ffc8d4936e1941313e
Mr.Phoenix <mrphoenixx@mail.ru>
2020-01-15 15:03:28 +0400

    Added enum parsing. Refactored code

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const request = require('request').defaults({
         'User-Agent': 'Docs Scraper'
     }
 });
-const fs = require("fs")
+const fs = require("fs");
 
-const RATE_LIMIT = 20   // Amout of simultaneously running requests
-const ERROR_TIMEOUT = 8 // Cooldown period if got error during request
+const RATE_LIMIT = 20;   // Amout of simultaneously running requests
+const ERROR_TIMEOUT = 8; // Cooldown period if got error during request
 const LUA_STATES = ['server', 'client', 'shared', 'menu'];
 
 function parseFuncPage(url, callback) {
@@ -16,9 +16,9 @@ function parseFuncPage(url, callback) {
             callback('Cant get: ' + url);
             return;
         }
-        let url_slit = url.split("/")
-        let name = url_slit[url_slit.length - 1]
-        let scope = url_slit[url_slit.length - 2]
+        let url_slit = url.split("/");
+        let name = url_slit[url_slit.length - 1];
+        let scope = url_slit[url_slit.length - 2];
         let $ = cheerio.load(html);
         let data = {
             name: name,
@@ -42,11 +42,11 @@ function parseFuncPage(url, callback) {
             data.deprecated = true;
         }
         if (categories.indexOf("class_functions") != -1){
-            data.classFunction = true
-            data.fullname = scope+":"+name
+            data.classFunction = true;
+            data.fullname = scope+":"+name;
         } else if(categories.indexOf("category:hooks")) {
             data.isHook = true
-            data.fullname = scope+":"+name
+            data.fullname = scope+":"+name;
         } else {
             data.fullname = scope == "global" ? name : scope+"."+name;
         }
@@ -68,7 +68,7 @@ function parseFuncPage(url, callback) {
         }
         data.description = data.description.trim();
         // data.description = $("#Description").parent().nextUntil("h1").text().slice(0,-1)
-        $(".argument").each(function (index, elem) {
+        $(".argument").each((index, elem) => {
             let desc = $("div", elem).text().trim();
             let type = $("p > .arg_chunk > a", elem).text();
             let name = $("p > .arg_chunk > a", elem)[0].next.data.trim();
@@ -82,9 +82,9 @@ function parseFuncPage(url, callback) {
                 argData.name = split[0];
                 argData.defaultValue = split[1];
             }
-            data.argsuments.push(argData)
+            data.argsuments.push(argData);
         })
-        $(".return").each(function (index, elem) {
+        $(".return").each((index, elem) => {
             let type = $("p > span > a", elem).text();
             let desc = $("div", elem).text().trim();
             data.returns.push({
@@ -92,7 +92,7 @@ function parseFuncPage(url, callback) {
                 desc: desc
             })
         })
-        $(".examples_number").each(function (index, elem) {
+        $(".examples_number").each((index, elem) => {
             elem = $(elem);
             let desc = elem.next("p").text().trim();
             let output = elem.nextUntil(".first_example").last().text().trim(); // will probably break if has picture or anything else
@@ -108,7 +108,37 @@ function parseFuncPage(url, callback) {
     });
 }
 
-function parseListPage(url, callback) {
+function parseEnumPage(url, callback) {
+    request(url, (err, resp, html) => {
+        if (err) {
+            callback('Cant get: ' + url);
+            return;
+        }
+        let url_slit = url.split("/");
+        let name = url_slit[url_slit.length - 1];
+        let $ = cheerio.load(html);
+        let rows = $(".wikitable > tbody > tr");
+        let data = {
+            name: name,
+            entires: []
+        };
+        rows.each((index, elem) => {
+            elem = $(elem);
+            if(index == 0 ) {
+                return; // Skip header
+            }
+            let table_data = elem.children("td");
+            data.entires.push({
+                name  : $(table_data[0]).text().trim(),
+                value : $(table_data[1]).text().trim(),
+                desc  : $(table_data[2]).text().trim()
+            })
+        })
+        callback(null, data);
+    } )
+}
+
+function parseListPage(url, parserFunc, callback) {
     request(url, function (err, resp, html) {
         if(err) {
             callback(err);
@@ -119,9 +149,9 @@ function parseListPage(url, callback) {
         $("ul > li > a","#mw-pages").each(function(index, elem) {
             pages.push("https://wiki.garrysmod.com"+elem.attribs.href);
         })
-        let total_count = pages.length
-        console.log(`Parsing ${total_count} pages from ${url}`)
-        let entires = []
+        let total_count = pages.length;
+        console.log(`Parsing ${total_count} pages from ${url}`);
+        let entires = [];
         let done = false;
         let shiftAndRequest = function() {
             if(pages.length == 0){
@@ -132,33 +162,55 @@ function parseListPage(url, callback) {
                 return;
             }
             let page = pages.shift();
-            parseFuncPage(page,function(err, data){
+            parserFunc(page,(err, data) => {
                 if(err) {
                     // console.log("Error geting " + page + " retrying in "+ERROR_TIMEOUT+" seconds")
                     setTimeout(shiftAndRequest, ERROR_TIMEOUT * 1000);
                     return;
                 }
                 entires.push(data);
-                shiftAndRequest()
+                shiftAndRequest();
             })
             process.stdout.write(`Processing: ${entires.length}/${total_count} (${Math.round((entires.length/total_count) * 100)}%)\r`);
         }
-        for(i = 1; i < RATE_LIMIT ; i ++) {
-            shiftAndRequest()
+        for(i = 0; i < RATE_LIMIT ; i ++) {
+            shiftAndRequest();
         }
     } )
 }
 
-parseListPage("https://wiki.garrysmod.com/page/Category:Functions",function(err,entires){
-    let jsonData = JSON.stringify(entires)
-    let file = fs.openSync("functions.json", 'w');
-    fs.writeSync(file, jsonData)
-    fs.closeSync(file)
-    console.log("All functions are parsed, parsing hooks")
-    parseListPage("https://wiki.garrysmod.com/page/Category:Hooks", function(err, entires) {
+function parseAndWrite(url, filename, pageParser, callback) {
+    parseListPage(url, pageParser, (err,entires) => {
+        if(err) {
+            callback(`Cant parse ${url}: ${err}`);
+            return;
+        }
         let jsonData = JSON.stringify(entires)
-        let file = fs.openSync("hooks.json", 'w');
-        fs.writeSync(file, jsonData)
-        fs.closeSync(file)
-    } )
-})
+        let file = fs.openSync(filename, 'w');
+        fs.writeSync(file, jsonData);
+        fs.closeSync(file);
+        callback(err, entires);
+    } );
+}
+
+let queue = [
+    ["https://wiki.garrysmod.com/page/Category:Enumerations", "enums.json" , parseEnumPage, ((err,entires) => {if(!err){console.log("Done parsing enums!");}})],
+    ["https://wiki.garrysmod.com/page/Category:Functions", "funcstions.json" ,parseFuncPage, (err,entires) => {if(!err){console.log("Done parsing functions!");}}],
+    ["https://wiki.garrysmod.com/page/Category:Hooks", "hooks.json" ,parseFuncPage, (err,entires) => {if(!err){console.log("Done parsing hook!");}}],
+]
+
+function processQueue() {
+    if(queue.length == 0){
+        return;
+    }
+    let elem = queue.shift();
+    parseAndWrite(elem[0], elem[1], elem[2], (err, data) => {
+        elem[3](err, data);
+        if(err) {
+            return;
+        }
+        processQueue();
+    })
+}
+
+processQueue();
